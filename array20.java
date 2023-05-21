a8b04c630694658a956003a8061a47617fe21c86
Andrea <a_andrea.lozano.mercado@iespablopicasso.es>
2023-05-21 18:52:27 +0200

    Añadido el método para imprimir el contenido del array por pantalla

--- array20.java
+++ array20.java
@@ -20,4 +20,12 @@ public class array20 {
 
         return array;
     }
+
+    public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+        System.out.print(array[i] + " ");
+    }
+    System.out.println();
+}
+
 }
